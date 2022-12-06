be1582964275033b04737a990fe37072b0efcd95
taktarovgv <taktarovgv@gmail.com>
2022-12-06 12:19:07 +0700

    initial_1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import {useEffect} from "react";
 
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
+//
+//     useEffect(() => {
+//         tg.ready();
+//     }, [])
+    const onClose = () => tg.close()
 
-    useEffect(() => {
-        tg.ready();
-    }, [])
-    const onClose = () => {
-        tg.close()
-    }
 function App() {
   return (
     <div className="App">
-     My Work
-        <button onClick={onClose}>Закрыть</button>
+            My Work
+        <button onClick={onClose}> Закрыть</button>
     </div>
   );
 }
